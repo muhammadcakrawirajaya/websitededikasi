0b3376ac0537776bc10bd4e408d2a3f9fa35e754
Cakra Wirajaya <cakrawirajaya@Cakras-MacBook-Air.local>
2024-05-17 05:29:29 +0700

    about us finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-// This is script file
+// ABOUT-TAB
+var tabButton = document.getElementsByClassName('tab'),
+    tabContent = document.getElementsByClassName('tab-content');
 
+tabButton[0].classList.add('active');
+tabContent[0].style.display = 'block';
+
+function city(e, x) {
+    var i;
+    for (i = 0; i < tabButton.length; i++) {
+        tabContent[i].style.display = 'none';
+        tabButton[i].classList.remove('active');
+    }
+    document.getElementById(x).style.display = 'block';
+    e.currentTarget.classList.add('active');
+}
+
+
+// TESTIMONIAL
 $('.testimonials-container').owlCarousel({
     loop:true,
     autoplay:false,
--- section/about.php
+++ section/about.php
@@ -1,11 +1,65 @@
 <section class="about">
     <div class="about-content">
-        <img src="<?= ROOT_URL ?>assets/images/about.png" alt="about">
-        <div class="about-text">
-            <h1>Tentang Kami</h1>
-            <h5>Denah Pengabdi Kangsa Pertiwi</h5>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio eligendi blanditiis culpa veniam quae. Sed cumque in possimus, aliquam fugiat assumenda officiis officia qui illo aperiam, pariatur vero debitis? Blanditiis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex facilis quia voluptatum rem laudantium. Rerum nemo dicta quas, modi corrupti labore tenetur vero illo sint minus eum in veniam iste.</p>
-            <button type="button">Hire Me</button>
-        </div>
+        <img src="<?= ROOT_URL ?>assets/images/about.png" alt="about" class="about-image">
+
+
+
+        <!-- tabs -->
+<div class="about-tabs">
+    <div class="title-container">
+        <p>Tentang Kami</p>
+        <h1 class="subtitle">Pemuda & Pemudi Pengabdi</h1>
+        <h1 class="title">Kangsa Pertiwi</h1>
+        <img src="<?= ROOT_URL ?>assets/icon/shape1.png" alt="" class="title-icon">
+    </div>
+    <div class="tab-container">
+        <button class="tab" onclick="city(event, 'London')"><p>Tentang Kami</p></button>
+        <button class="tab" onclick="city(event, 'Paris')"><p>Visi & Misi</p></button>
+        <button class="tab" onclick="city(event, 'Tokyo')"><p>Filosofi Logo</p></button>
+    </div>
+    <div class="tab-content" id="London">
+        <p><b>Denah Pengabdi Kangsa Pertiwi </b>merupakan sebuah organisasi non-pemerintah yang bertekad untuk memberikan kontribusi positif bagi masyarakat. Dengan fokus utama pada pengabdian masyarakat, Denah Pengabdi Kangsa Pertiwi menjalankan beragam program dan inisiatif yang dirancang untuk memperbaiki kondisi sosial, ekonomi, dan lingkungan di sekitarnya.
+        <br>
+        Selaras dengan visi dan misinya, Denah Pengabdi Kangsa Pertiwi senantiasa menunjukkan dedikasi yang tak kenal lelah dalam upaya mereka untuk membangun masyarakat yang lebih baik. Mereka adalah pionir dalam mempromosikan nilai-nilai kepedulian, keadilan, dan keberlanjutan, serta menjadi teladan bagi orang lain dalam memperjuangkan perubahan positif bagi negeri tercinta, Indonesia.</p>
+    </div>
+    <div class="tab-content" id="Paris">
+        <h1 class="visi">Visi Kami</h1>
+        <p class="visi-desc">Menciptakan wadah untuk mendorong pemuda pemudi indonesia menjadi garda terdepan perubahan guna memberikan kebermanfaatan bagi masyarakat melalui program pengabdian</p>
+        <h1 class="misi-title">Misi Kami</h1>
+<div class="misi-row">
+  <div class="misi-col">
+    <ul>
+      <li>Melaksanakan program kerja melalui berbagai bidang kegiatan dengan relevan dengan permasalahan sekitar</li>
+      <li>Menjalin hubungan dengan stakeholder terkait untuk menghadirkan kegiatan yang harmonis</li>
+      <li>Memberdayakan masyarakat sekitar dalam setiap kegiatan</li>
+      <li>memberdayakan potensi pemuda-pemudi melalui inovasi dan berkomitmen untuk memberikan hak pendidikan guna menciptakan generasi yang berpengetahuan luas, kreatif, dan berkomitmen untuk memajukan kualitas masyarakatnya</li>
+     </ul>
+  </div>
+</div>
+    </div>
+    <div class="tab-content" id="Tokyo">
+    <img src="<?= ROOT_URL ?>assets/icon/original_logo_hitam.png" alt="logo" class="filosofi-logo">
+    <div class="filosofi-row">
+  <div class="filosofi-col">
+    <ul>
+      <li>Melaksanakan program kerja melalui berbagai bidang kegiatan dengan relevan dengan permasalahan sekitar</li>
+      <li>Menjalin hubungan dengan stakeholder terkait untuk menghadirkan kegiatan yang harmonis</li>
+      <li>Memberdayakan masyarakat sekitar dalam setiap kegiatan</li>
+      <li>memberdayakan potensi pemuda-pemudi melalui inovasi dan berkomitmen untuk memberikan hak pendidikan guna menciptakan generasi yang berpengetahuan luas, kreatif, dan berkomitmen untuk memajukan kualitas masyarakatnya</li>
+     </ul>
+  </div>
+</div>
+        <p class="filosofi-desc">Secara keseluruhan, logo tersebut mencerminkan semangat, kepedulian, pertumbuhan, dan kontribusi yang berkelanjutan dalam pengabdian masyarakat</p>
+    </div>
+</div>
+        <!--/ tabs -->
+
+
+
+
+
+
+
+        
     </div>
 </section>
