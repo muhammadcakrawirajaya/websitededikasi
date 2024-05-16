28df37a87cf9a3d0e292ffd77391a8ec91d3c711
Cakra Wirajaya <cakrawirajaya@Cakras-MacBook-Air.local>
2024-05-16 16:02:46 +0700

    program section finished

--- section/program.php
+++ section/program.php
@@ -1,22 +1,27 @@
-<div class="wrapper">
-<div class="card">
-	<h3 class="card-title">CARD TITLE</h3>
-		<p class="card-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-		<button class="card-btn">READ MORE</button>
+<section class="program">
+	<h3 class="subtitle">Kategori</h3>
+    <h1 class="title">Program Kegiatan<span class="bold"> DEDIKASI</span></h1>
+    <img src="<?= ROOT_URL ?>assets/icon/shape1.png" alt="" class="title-icon">
+	<div class="program-wrapper">
+	<div class="card satu">
+	<i class="fa-solid fa-seedling"></i>
+		<h3 class="card-title">SELF FUNDED</h3>
+			<p class="card-content">Dalam program ini, peserta bertanggung jawab sepenuhnya atas semua biaya terkait, termasuk biaya pendaftaran, transportasi, akomodasi, dan biaya lainnya. Mereka perlu menyediakan dana sendiri untuk mengikuti program tanpa bantuan keuangan dari penyelenggara.</p>
+		</div>
+		<div class="card dua">
+		<i class="fa-solid fa-hand-holding-dollar"></i>
+			<h3 class="card-title">PARTIAL FUNDED</h3>
+			<p class="card-content">Program ini menawarkan dukungan keuangan sebagian untuk biaya-biaya terkait, seperti biaya pendaftaran atau transportasi. Namun, peserta diharapkan untuk menyediakan sebagian dari biaya tersebut sendiri atau mencari sumber pendanaan tambahan.</p>
+		</div>
+		<div class="card tiga">
+			<i class="fa-solid fa-sack-dollar"></i>
+			<h3 class="card-title">FUNDED - REGULER</h3>
+			<p class="card-content">Program ini menyediakan dana penuh untuk peserta, termasuk transportasi, akomodasi, dan biaya lainnya yang terkait. Peserta tidak perlu mengeluarkan biaya tambahan selama program berlangsung.</p>
+		</div>
+		<div class="card empat">
+		<i class="fa-solid fa-gift"></i>
+			<h3 class="card-title">FUNDED - PREMIUM</h3>
+			<p class="card-content">Sebagai versi premium dari program funded, program ini tidak hanya menyediakan dana penuh untuk biaya-biaya terkait, tetapi juga menawarkan fasilitas tambahan atau pengalaman eksklusif kepada peserta.</p>
+		</div>
 	</div>
-	<div class="card">
-		<h3 class="card-title">CARD TITLE</h3>
-		<p class="card-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-		<button class="card-btn">READ MORE</button>
-	</div>
-	<div class="card">
-		<h3 class="card-title">CARD TITLE</h3>
-		<p class="card-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-		<button class="card-btn">READ MORE</button>
-	</div>
-	<div class="card">
-		<h3 class="card-title">CARD TITLE</h3>
-		<p class="card-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-		<button class="card-btn">READ MORE</button>
-	</div>
-</div>
+</section>
